6ae8885192cb0ff1d887f2e6808db82e9ec62277
TreyRubino <115559658+TreyRubino@users.noreply.github.com>
2024-11-30 20:37:47 -0500

    Delete src directory

--- src/start_client.py
+++
@@ -1,8 +0,0 @@
-from inc.Controller import ClientController
-
-try:
-    client_controller = ClientController()
-    client_controller.start()
-except Exception as e:
-    print(f"An error has occurred: {e}")
-    client_controller.stop()
--- src/start_server.py
+++
@@ -1,9 +0,0 @@
-from inc.Controller import ServerController
-
-# maybe this can be the 'View'
-try:
-    server_controller = ServerController()
-    server_controller.start()
-except Exception as e:
-    print(f"An error has occurred: {e}")
-    server_controller.stop()
